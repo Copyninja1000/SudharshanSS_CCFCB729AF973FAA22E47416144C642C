596d4cbfaab82f3701b7d54d8013e1717461392a
Copyninja1000 <144534865+Copyninja1000@users.noreply.github.com>
2023-09-10 17:16:40 +0530

    Update main (8).py

--- challenge unit 3/main (8).py
+++ challenge unit 3/main (8).py
@@ -1,115 +1,26 @@
-print("-----Program for Student Information-----")
- 
-D = dict()
- 
-n = int(input('How many student record you want to store?? '))
- 
-# Add student information
-# to the dictionary
-for i in range(0,n):
-    x, y = input("Enter the complete name (First and last name) of student: ").split()
-    z = input("Enter contact number: ")
-    m = input('Enter Marks: ')
-    D[x, y] = (z, m)
-     
-# define a function for shorting
-# names based on first name
-def sort():
-    ls = list()
-    # fetch key and value using
-    # items() method
-    for sname,details in D.items():
-       
-        # store key parts as an tuple
-        tup = (sname[0],sname[1])
-         
-        # add tuple to the list
-        ls.append(tup)   
-         
-    # sort the final list of tuples
-    ls = sorted(ls)   
-    for i in ls:
-       
-        # print first name and second name
-        print(i[0],i[1])
-    return
-   
-# define a function for
-# finding the minimum marks
-# in stored data
-def minmarks():
-    ls = list()
-    # fetch key and value using
-    # items() methods
-    for sname,details in D.items():
-        # add details second element
-        # (marks) to the list
-        ls.append(details[1])   
-     
-    # sort the list elements   
-    ls = sorted(ls)   
-    print("Minimum marks: ", min(ls))
-     
-    return
-   
-# define a function for searching
-# student contact number
-def searchdetail(fname):
-    ls = list()
-     
-    for sname,details in D.items():
-       
-        tup=(sname,details)
-        ls.append(tup)
-         
-    for i in ls:
-        if i[0][0] == fname:
-            print(i[1][0])
-    return
-   
-# define a function for
-# asking the options
-def option():
-   
-    choice = int(input('Enter the operation detail: \n \
-    1: Sorting using first name \n \
-    2: Finding Minimum marks \n \
-    3: Search contact number using first name: \n \
-    4: Exit\n \
-    Option: '))
-     
-    if choice == 1:
-        # function call
-        sort()
-        print('Want to perform some other operation??? Y or N: ')
-        inp = input()
-        if inp == 'Y':
-            option()
-             
-        # exit function call   
-        exit()
-         
-    elif choice == 2:
-        minmarks()
-        print('Want to perform some other operation??? Y or N: ')
-         
-        inp = input()
-        if inp == 'Y':
-            option()
-        exit()
-         
-    elif choice == 3:
-        first = input('Enter first name of student: ')
-        searchdetail(first)
-         
-        print('Want to perform some other operation??? Y or N: ')
-        inp = input()
-        if inp == 'Y':
-            option()
-             
-        exit()
-    else:
-        print('Thanks for executing me!!!!')
-        exit()
-         
-option()
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the student list based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Test the function with different input lists of students
+if __name__ == "__main__":
+    # Example student objects
+    students = [
+        Student("Alice", "101", 3.9),
+        Student("Bob", "102", 3.7),
+        Student("Charlie", "103", 4.0),
+        Student("David", "104", 3.8),
+    ]
+
+    sorted_students = sort_students(students)
+
+    # Print the sorted list of students
+    for student in sorted_students:
+        print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}") 
